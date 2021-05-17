d101cb0c0113e8fa30da45a8325e421ae4042285
Thomas Klebel <t.klebel@hotmail.com>
2021-05-17 13:29:05 +0200

    start exploring levels of OA

---
+++ 11-levels-of-oa.R
@@ -0,0 +1,155 @@
+library(tidyverse)
+
+con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "data/processed/sdg_mag.db")
+
+DBI::dbListTables(con)
+
+
+affils <- tbl(con, "affils_w_country")
+author_metadata <- tbl(con, "author_metadata")
+author_paper_affiliations <- tbl(con, "author_paper_affil")
+papers <- tbl(con, "papers")
+funded_projects <- tbl(con, "funded_projects")
+wb_indicators <- tbl(con, "world_bank_indicators")
+leiden <- tbl(con, "leiden_ranking")
+
+
+
+# explore duplicates ------
+papers %>%
+  distinct(paperid) %>%
+  count()
+
+# todo: fix look into this discrepancy
+duplicated_papers <- papers %>%
+  count(paperid) %>%
+  filter(n > 1) %>%
+  collect()
+# questions: how do some have an NA paperid?
+# why are there duplicates?
+papers %>%
+  filter(paperid == 295256) %>%
+  collect() %>%
+  View()
+# one reason for "duplication" is being relevant to multiple fos (virus being
+# part of Medicine, mainly)
+# but to some part there are real duplicates here
+papers %>%
+  filter(paperid == 295256) %>%
+  collect() %>%
+  distinct()
+
+
+
+# aggregate oa_status
+oa_status <- papers %>%
+  filter(!is.na(is_oa)) %>%
+  select(paperid, fos_displayname, year, is_oa, oa_status) %>%
+  distinct()
+
+
+oa_per_year <- oa_status %>%
+  count(fos_displayname, year, is_oa) %>%
+  collect()
+
+oa_status_per_year <- oa_status %>%
+  count(fos_displayname, year, oa_status) %>%
+  collect()
+
+oa_per_year %>%
+  mutate(oa_share = n/sum(n)) %>%
+  filter(is_oa == 1) %>%
+  ggplot(aes(as.factor(year), oa_share, colour = fos_displayname,
+             group = fos_displayname)) +
+  geom_point() +
+  geom_line() +
+  scale_y_continuous(labels = function(x) scales::percent(x, accuracy = 1)) +
+  labs(x = NULL, title = "OA share by Field of Study",
+       y = NULL, colour = NULL) +
+  theme(legend.position = "top")
+
+
+oa_status_per_year %>%
+  filter(oa_status %in% c("bronze", "closed", "gold", "green", "hybrid")) %>%
+  mutate(oa_share = n/sum(n)) %>%
+  ggplot(aes(as.factor(year), oa_share, colour = oa_status,
+             group = oa_status)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(vars(fos_displayname)) +
+  scale_y_continuous(labels = function(x) scales::percent(x, accuracy = 1)) +
+  labs(x = NULL, title = "OA share by Field of Study",
+       y = NULL, colour = NULL) +
+  theme(legend.position = "top")
+
+# swapping facets with oclours
+oa_status_per_year %>%
+  filter(oa_status %in% c("bronze", "closed", "gold", "green", "hybrid")) %>%
+  mutate(oa_share = n/sum(n)) %>%
+  ggplot(aes(as.factor(year), oa_share, colour = fos_displayname,
+             group = fos_displayname)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(vars(oa_status)) +
+  scale_y_continuous(labels = function(x) scales::percent(x, accuracy = 1)) +
+  labs(x = NULL, title = "OA share by Field of Study",
+       y = NULL, colour = NULL) +
+  theme(legend.position = "top")
+
+# the rise in oa is thus mainly due to rise in gold OA, slightly rise in hybrid
+# unpaywall might always prefer gold over green
+
+## OA per funder ------
+paper_oa_flag <- papers %>%
+  select(paperid, is_oa, oa_status, year, fos_displayname)
+
+oa_per_funder <- funded_projects %>%
+  left_join(paper_oa_flag) %>%
+  collect()
+
+oa_per_funder
+
+oa_per_funder %>%
+  count(funder_name, sort = TRUE) %>% View()
+
+oa_per_funder_aggregated <- oa_per_funder %>%
+  select(doi, funder_name, year, is_oa) %>%
+  distinct() %>% # remove duplicate rows since many papers are funded by multiple projects
+  group_by(year, funder_name) %>%
+  count(is_oa) %>%
+  mutate(is_oa = recode(is_oa, `0` = "no", `1` = "yes", .missing = "unknown")) %>%
+  filter(is_oa != "unknown") %>%
+  mutate(oa_share = n/sum(n),
+         total_papers = sum(n))
+
+# why do so many have no year?
+# the issue was that I only joined the climate papers
+
+# -> obvs the results are now heavily influenced by medicine
+
+text_labels <- oa_per_funder_aggregated %>%
+  group_by(funder_name) %>%
+  summarise(nn_papers = sum(total_papers)) %>%
+  mutate(label = glue::glue("n = {format(nn_papers, big.mark = '.', decimal.mark = ',')}"))
+
+
+oa_per_funder_aggregated %>%
+  filter(is_oa == "yes") %>%
+  ggplot(aes(lubridate::ymd(year, truncated = 2L), oa_share,
+             group = funder_name)) +
+  geom_point() +
+  geom_line() +
+  geom_text(aes(x = lubridate::ymd("2017-01-01"),
+                y = .4,
+                label = label),
+            data = text_labels) +
+  facet_wrap(vars(str_wrap(funder_name, 50))) +
+  scale_y_continuous(labels = function(x) scales::percent(x, accuracy = 1)) +
+  labs(x = NULL, title = "OA share by funder",
+       y = NULL, colour = NULL) +
+  theme(legend.position = "top")
+
+# share of OA going down for CA institutes of health
+
+
+## OA per country
